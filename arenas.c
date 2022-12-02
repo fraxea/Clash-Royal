8f594e638609b97002e09a82243ffd7dd91b641c
fraxea <119697229+fraxea@users.noreply.github.com>
2022-12-03 00:49:52 +0330

    Update arenas.c

--- arenas.c
+++ arenas.c
@@ -6,6 +6,8 @@ int main()
 	//This version has inputs!!!
 	int a;
 	scanf("%i", &a);
+	if (a < 1)
+		printf("Invalid number!");
 	for (int i = 0; i < a; i++)
 		printf("Arena %i\n", i + 1);
 	return 0;
