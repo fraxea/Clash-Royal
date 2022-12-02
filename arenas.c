7829bbe443144235fc19bfce9c5edcb6c3179f51
fraxea <119697229+fraxea@users.noreply.github.com>
2022-12-03 00:38:14 +0330

    Update arenas.c

--- arenas.c
+++ arenas.c
@@ -3,7 +3,10 @@
 
 int main()
 {
-	for (int i = 0; i < 10; i++)
+	//This version has inputs!!!
+	int a;
+	scanf("%i", &a);
+	for (int i = 0; i < a; i++)
 		printf("Arena %i\n", i + 1);
 	return 0;
 }
